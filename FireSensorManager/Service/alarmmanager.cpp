2c63921d1268b75fbc67197d05ee0e2eba26da26
Xardas97 <markomicovic19@yahoo.com>
2022-07-06 17:11:59 +0200

    AlarmManager | Add support for multiple alarms and custom volume

--- FireSensorManager/Service/alarmmanager.cpp
+++ FireSensorManager/Service/alarmmanager.cpp
@@ -6,16 +6,22 @@
 AlarmManager::AlarmManager()
     : m_player{new QMediaPlayer()}
 {
-    m_player->setLoops(QMediaPlayer::Loops::Infinite);
+    m_chosenVolume = 0.25;
+    m_chosenAlarm = Alarm::Classic;
 }
 
 void AlarmManager::play()
 {
+    play(m_chosenAlarm, QMediaPlayer::Loops::Infinite);
+}
+
+void AlarmManager::play(Alarm alarm, int loops)
+{
     if (m_player->playbackState() != QMediaPlayer::StoppedState)
         stop();
 
-    repickAlarm();
-    remakeOutput();
+    repickAlarm(alarm);
+    remakeOutput(m_chosenVolume);
 
     if (!m_alarm)
     {
@@ -23,22 +29,25 @@ void AlarmManager::play()
         return;
     }
 
+    m_player->setLoops(loops);
     m_player->setAudioOutput(m_output.get());
     m_player->setSourceDevice(m_alarm.get());
 
     m_player->play();
 }
 
-void AlarmManager::repickAlarm()
+void AlarmManager::repickAlarm(Alarm alarm)
 {
     if (m_player->playbackState() != QMediaPlayer::StoppedState)
         return;
 
-    QFile file(R"(:/Resources/Sounds/ClassicAlarm.wav)");
+    auto alarmPath = getAlarmPath(alarm);
+
+    QFile file(alarmPath);
     file.open(QIODevice::ReadOnly);
     if (!file.isOpen())
     {
-        qDebug() << "Failed to open the alarm file";
+        qDebug() << "Failed to open the alarm file at " << alarmPath;
         return;
     }
 
@@ -56,16 +65,54 @@ void AlarmManager::repickAlarm()
     m_alarm = std::move(buffer);
 }
 
-void AlarmManager::remakeOutput()
+QString AlarmManager::getAlarmPath(Alarm alarm)
+{
+    return R"(:/Resources/Sounds/)" + getAlarmName(alarm) + ".wav";
+}
+
+QString AlarmManager::getAlarmName(Alarm alarm)
+{
+    switch (alarm)
+    {
+        case Alarm::Classic:       return "ClassicAlarm";
+        case Alarm::DigitalClock:  return "DigitalClockAlarm";
+        case Alarm::Buzzer:        return "BuzzerAlarm";
+        case Alarm::Facility:      return "FacilityAlarm";
+        case Alarm::Vintage:       return "VintageAlarm";
+        default:                   return QString {};
+    }
+}
+
+void AlarmManager::remakeOutput(float volume)
 {
     if (m_player->playbackState() != QMediaPlayer::StoppedState)
         return;
 
     m_output = std::make_unique<QAudioOutput>();
-    m_output->setVolume(100);
+    m_output->setVolume(volume);
 }
 
 void AlarmManager::stop()
 {
     m_player->stop();
+}
+
+Alarm AlarmManager::alarm()
+{
+    return m_chosenAlarm;
+}
+
+void AlarmManager::setAlarm(Alarm alarm)
+{
+    m_chosenAlarm = alarm;
+}
+
+float AlarmManager::volume()
+{
+    return m_chosenVolume;
+}
+
+void AlarmManager::setVolume(float volume)
+{
+    m_chosenVolume = volume;
 }
--- FireSensorManager/Service/alarmmanager.h
+++ FireSensorManager/Service/alarmmanager.h
@@ -7,6 +7,8 @@
 #include <QMediaPlayer>
 #include <QAudioOutput>
 
+enum Alarm { Classic, DigitalClock, Buzzer, Facility, Vintage };
+
 class AlarmManager : public QObject
 {
     Q_OBJECT
@@ -15,11 +17,23 @@ public:
 
 public slots:
     void play();
+    void play(Alarm alarm, int loops);
     void stop();
 
+    Alarm alarm();
+    void  setAlarm(Alarm alarm);
+    float volume();
+    void  setVolume(float volume);
+
 private:
-    void repickAlarm();
-    void remakeOutput();
+    void repickAlarm(Alarm alarm);
+    void remakeOutput(float volume);
+
+    QString getAlarmPath(Alarm alarm);
+    QString getAlarmName(Alarm alarm);
+
+    float m_chosenVolume;
+    Alarm m_chosenAlarm;
 
     std::unique_ptr<QBuffer> m_alarm;
     std::unique_ptr<QAudioOutput> m_output;
