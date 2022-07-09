5f7a6627c2178237919cccfd3124a2b201e2eab1
Xardas97 <markomicovic19@yahoo.com>
2022-07-09 18:44:33 +0200

    Sensor | Increase max sensors per IP to 10

--- Common/ports.h
+++ Common/ports.h
@@ -7,7 +7,7 @@ class Ports
         static const unsigned short base_sensor_port = 56000;
         static const unsigned short sensor_detector_port = 56560;
 
-        static const unsigned short max_sensor_ports = 5;
+        static const unsigned short max_sensor_ports = 10;
 };
 
 #endif // PORTS_H
